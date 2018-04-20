2861458e916afb627c6579489fac93f83e406033
binarytrance <ganeshan.dash@gmail.com>
2018-04-20 17:41:47 +0530

    added support for uncss

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,12 @@ var sass               = require('gulp-sass');
 var sourcemaps         = require('gulp-sourcemaps');
 var uglify             = require('gulp-uglify');
 var babel              = require('gulp-babel');
-var csslint = require('gulp-csslint');
+// var uncss              = require('gulp-uncss');
+var postcss            = require('gulp-postcss');
+var uncss              = require('postcss-uncss');
+var csslint            = require('gulp-csslint');
 var sassLint 		   = require('gulp-sass-lint');
-var jshint           = require('gulp-jshint');
+var jshint             = require('gulp-jshint');
 // sudo npm install gulp-uglify browser-sync gulp-plumber gulp-autoprefixer gulp-sass gulp-pug gulp-imagemin gulp-cache gulp-clean-css gulp-sourcemaps gulp-concat beeper gulp-util gulp-rename gulp-notify --save-dev
 var jsVendorFiles      = ['js/vendor/*.js'];             // Holds the js vendor files to be concatenated
 var myJsFiles          = ['js/*.js'];    // Holds the js files to be concatenated
@@ -34,30 +37,24 @@ var onError            = function(err) { // Custom error msg with beep sound and
 
 
 gulp.task('styles', function() {
+	var plugins = [
+        uncss({
+         ignore: [
+             /.js/
+             ],
+           html: ['build/**/*.html', 'build/**/*.js'],
+
+       }),
+    ];
 	gulp.src('styles/*.scss')
 	.pipe(plumber({ errorHandler: onError }))
-	// .pipe(sassLint({
- //      options: {
- //        formatter: 'stylish',
- //        'merge-default-rules': false
- //      },
- //      files: {ignore: ''},
- //      rules: {
- //        'no-ids': 2,
- //        'no-mergeable-selectors': 0,
- //      },
- //      configFile: '.sass-lint.yml'
- //    }))
-    // .pipe(sassLint.format())
-    // .pipe(sassLint.failOnError())
 	.pipe(sourcemaps.init())
 	.pipe(sass({indentedSyntax: true}))
 	.pipe(autoprefixer({
 		browsers: ['last 5 versions'],
 		cascade: false}))
-	// .pipe(csslint())
- //    .pipe(csslint.formatter());
 	.pipe(cleanCSS())
+	.pipe(postcss(plugins))
 	.pipe(sourcemaps.write())
 	.pipe(rename({ suffix: '.min'}))
 	.pipe(gulp.dest('build/css'));
